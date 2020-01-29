94ea3c2e85e804952f832675bc08157c9bd6c0a0
jbalexjb <60420317+jbalexjb@users.noreply.github.com>
2020-01-29 13:59:09 +0530

    Add files via upload

---
+++ randomSample.py
@@ -0,0 +1,13 @@
+import random
+
+def fun():
+    n = int(input("Enter a Number"))
+    if(n == r):
+        print("Success")
+    else:
+        print("Failed")
+        fun()
+r =random.randrange(0,5)
+#print(r)
+fun()
+
